6df60319f32c2cfa83a005e3febd40684dea9976
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-12 17:25:24 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function volume_sphere() {
     //Write your code here
+	const radiusV = document.getElementById("radius").value;
+	const volume = (4/3)*3.14159*radiusV*radiusV*radiusV;
+	document.getElementById("volume").setAttribute("value", volume);;
   
 } 
 
