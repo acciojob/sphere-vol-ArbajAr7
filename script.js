3dae00cb139a9ed79a9d7438cbbed5650a17ed3a
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-12 17:43:34 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function volume_sphere() {
     //Write your code here
 	const radiusV = document.getElementById("radius").value;
-	const volume = (4/3)*3.14159*radiusV*radiusV*radiusV;
-	document.getElementById("volume").setAttribute("value", volume);;
-  
+
+	if(radiusV < 0 || isNaN(radiusV))
+	{
+		document.getElementById("volume").setAttribute("value", "NaN");
+	}
+	if(typeof radiusV == "string")
+	{
+		document.getElementById("volume").setAttribute("value", "NaN");
+	}
+	const volume = (4/3)*Math.PI*radiusV*radiusV*radiusV;
+	document.getElementById("volume").setAttribute("value", volume);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
